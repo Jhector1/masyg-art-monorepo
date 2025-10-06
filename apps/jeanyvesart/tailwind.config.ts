44f8404741654466a1e2f2e17486a31c2e5bb8f6
Jean Yves Hector <jeanyveshector@gmail.com>
2025-10-06 15:00:22 -0500

    dockerfile added

--- apps/jeanyvesart/tailwind.config.ts
+++ apps/jeanyvesart/tailwind.config.ts
@@ -1,15 +1,24 @@
 // apps/<app>/tailwind.config.ts
 import type { Config } from "tailwindcss";
 import presetImport from "@acme/tailwind-preset";
-
+import path from 'path'
+const r = (...p: string[]) => path.join(process.cwd(), ...p)
 // handle both ESM/CJS preset exports
 const preset = (presetImport as any)?.default ?? presetImport;
 
 export default {
   presets: [preset],
   content: [
-    "./src/**/*.{ts,tsx}",
-    "../../packages/ui/src/**/*.{ts,tsx,js,jsx}",
-    "../../packages/core/src/**/*.{ts,tsx,js,jsx}"
+    // "./src/**/*.{ts,tsx}",
+    // "../../packages/ui/src/**/*.{ts,tsx,js,jsx}",
+    // "../../packages/core/src/**/*.{ts,tsx,js,jsx}",
+
+      r('app/**/*.{js,ts,jsx,tsx,mdx}'),
+    r('src/**/*.{js,ts,jsx,tsx,mdx}'),
+
+    // shared workspaces used at runtime:
+    r('../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}'),
+    r('../../packages/core/**/*.{js,ts,jsx,tsx,mdx}'),
+    r('../../packages/server/**/*.{js,ts,jsx,tsx,mdx}'),
   ]
 } satisfies Config;
