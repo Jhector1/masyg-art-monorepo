798a654871dc1d84f665c0e8c99c4b70ca324905
Jean Yves Hector <jeanyveshector@gmail.com>
2025-10-06 01:36:08 -0500

    dockerfile added

--- apps/ziledigital/next.config.ts
+++ apps/ziledigital/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 const config: NextConfig = {
-  transpilePackages: ["@acme/ui", "@acme/core", "@acme/server", "@acme/config", "@acme/db"],
-    images: {
+  output: "standalone",
+
+  transpilePackages: [
+    "@acme/ui",
+    "@acme/core",
+    "@acme/server",
+    "@acme/config",
+    "@acme/db",
+  ],
+  images: {
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "lh4.googleusercontent.com" },
